bcb7397b2a291fd97d7f2c24d9deaa664599e800
jslee <etoileettoi@gmail.com>
2020-07-18 14:40:26 +0900

    프로필 표시 프래그먼트 보완

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200718/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200718/fragments/MyProfileFragment.kt
@@ -1,7 +1,25 @@
 package kr.co.tjoeun.pizzastore_20200718.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kr.co.tjoeun.pizzastore_20200718.R
+import java.util.zip.Inflater
 
 class MyProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
 }
